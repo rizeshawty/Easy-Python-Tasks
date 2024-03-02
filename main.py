d17482934b422d0ba05418480561671d45f3a1cd
Bochkarev DD <132708222+rizeshawty@users.noreply.github.com>
2024-03-02 10:32:21 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+for otrezok in range(185311, 185330+1):
+    massiv = []
+    for delenie in range(2, 185330+1):
+        if otrezok % delenie == 0:
+            massiv.append(delenie)
+            if len(massiv) == 4:
+              print(*massiv)
